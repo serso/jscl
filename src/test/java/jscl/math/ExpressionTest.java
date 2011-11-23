bf431f657de22cff61c13b66a28f317b0fe369a5
Sergey Solovyev <serso1988@gmail.com>
2011-11-23 16:11:41 +0400

    tests

--- src/test/java/jscl/math/ExpressionTest.java
+++ src/test/java/jscl/math/ExpressionTest.java
@@ -478,6 +478,7 @@ public class ExpressionTest {
 			//me.setDefaultNumeralBase(NumeralBase.bin);
 			Assert.assertEquals("10.0", me.evaluate("0b01010"));
 			Assert.assertEquals("10.0", me.evaluate("0b1010"));
+			Assert.assertEquals("520.0", me.evaluate("0o1010"));
 			Assert.assertEquals("1010.0", me.evaluate("1010"));
 			Assert.assertEquals("1010.1", me.evaluate("1010.1"));
 		} finally {
