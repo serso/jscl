4d80083ee2148fe2582b960ebb7e0298f59dff46
Sergey Solovyev <se.solovyev@gmail.com>
2012-02-10 22:41:38 +0400

    android_calculator-64: Sgn() problem: Sgn(0) = 1

--- src/main/java/jscl/math/function/Sgn.java
+++ src/main/java/jscl/math/function/Sgn.java
@@ -3,73 +3,92 @@ package jscl.math.function;
 import jscl.math.*;
 import jscl.math.JsclInteger;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class Sgn extends Function {
-    public Sgn(Generic generic) {
-        super("sgn",new Generic[] {generic});
-    }
 
-    public Generic antiDerivative(int n) throws NotIntegrableException {
-        return new Abs(parameters[0]).selfExpand();
-    }
+	public Sgn(Generic generic) {
+		super("sgn", new Generic[]{generic});
+	}
 
-    public Generic derivative(int n) {
-        return JsclInteger.valueOf(0);
-    }
+	public Generic antiDerivative(int n) throws NotIntegrableException {
+		return new Abs(parameters[0]).selfExpand();
+	}
 
-    public Generic selfExpand() {
-        if(parameters[0].signum()<0) {
-            return new Sgn(parameters[0].negate()).selfExpand().negate();
-        } else if(parameters[0].signum()==0) {
-            return JsclInteger.valueOf(1);
-        }
-        try {
-            return JsclInteger.valueOf(parameters[0].integerValue().signum());
-        } catch (NotIntegerException e) {}
-        return expressionValue();
-    }
+	public Generic derivative(int n) {
+		return JsclInteger.valueOf(0);
+	}
 
-    public Generic selfElementary() {
-        return new Fraction(
-            parameters[0],
-            new Abs(parameters[0]).selfElementary()
-        ).selfElementary();
-    }
+	public Generic selfExpand() {
+		final Generic result = selfEvaluate();
 
-    public Generic selfSimplify() {
-        if(parameters[0].signum()<0) {
-            return new Sgn(parameters[0].negate()).selfExpand().negate();
-        } else if(parameters[0].signum()==0) {
-            return JsclInteger.valueOf(1);
-        }
-        try {
-            return JsclInteger.valueOf(parameters[0].integerValue().signum());
-        } catch (NotIntegerException e) {}
-        try {
-            Variable v= parameters[0].variableValue();
-            if(v instanceof Abs) {
-                return JsclInteger.valueOf(1);
-            } else if(v instanceof Sgn) {
-                Function f=(Function)v;
-                return f.selfSimplify();
-            }
-        } catch (NotVariableException e) {}
-        return expressionValue();
-    }
+		if (result == null) {
+			return expressionValue();
+		} else {
+			return result;
+		}
 
-    public Generic selfNumeric() {
-        return ((NumericWrapper) parameters[0]).sgn();
-    }
+	}
 
-    public String toJava() {
-        StringBuffer buffer=new StringBuffer();
-        buffer.append(parameters[0].toJava());
-        buffer.append(".sgn()");
-        return buffer.toString();
-    }
+	@Nullable
+	private Generic selfEvaluate() {
+		Generic result = null;
 
-    @NotNull
+		if (parameters[0].signum() < 0) {
+			result = new Sgn(parameters[0].negate()).selfExpand().negate();
+		} else if (parameters[0].signum() == 0) {
+			result = JsclInteger.valueOf(0);
+		}
+
+		if (result == null) {
+			try {
+				result = JsclInteger.valueOf(parameters[0].integerValue().signum());
+			} catch (NotIntegerException e) {
+			}
+		}
+
+		return result;
+	}
+
+	public Generic selfElementary() {
+		return new Fraction(parameters[0], new Abs(parameters[0]).selfElementary()).selfElementary();
+	}
+
+	public Generic selfSimplify() {
+		final Generic result = selfEvaluate();
+
+		if (result == null) {
+			try {
+				Variable v = parameters[0].variableValue();
+				if (v instanceof Abs) {
+					return JsclInteger.valueOf(1);
+				} else if (v instanceof Sgn) {
+					Function f = (Function) v;
+					return f.selfSimplify();
+				}
+			} catch (NotVariableException e) {
+			}
+
+			return expressionValue();
+		} else {
+			return result;
+		}
+	}
+
+	public Generic selfNumeric() {
+		return ((NumericWrapper) parameters[0]).sgn();
+	}
+
+	public String toJava() {
+		final StringBuilder result = new StringBuilder();
+
+		result.append(parameters[0].toJava());
+		result.append(".sgn()");
+		return result.toString();
+	}
+
+	@NotNull
 	public Variable newInstance() {
-        return new Sgn(null);
-    }
+		return new Sgn(null);
+	}
 }
