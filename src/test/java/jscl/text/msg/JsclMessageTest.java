5a9c8feeadffd6cb290ff74b8202e34bee687997
Sergey Solovyev <se.solovyev@gmail.com>
2013-07-16 16:47:53 +0400

    polish test added

--- src/test/java/jscl/text/msg/JsclMessageTest.java
+++ src/test/java/jscl/text/msg/JsclMessageTest.java
@@ -1,10 +1,13 @@
 package jscl.text.msg;
 
-import junit.framework.Assert;
 import org.junit.Test;
-import org.solovyev.common.msg.MessageType;
 
 import java.util.Locale;
+
+import static java.util.Locale.ENGLISH;
+import static jscl.text.msg.Messages.msg_1;
+import static org.junit.Assert.assertTrue;
+import static org.solovyev.common.msg.MessageType.error;
 
 /**
  * User: serso
@@ -15,7 +18,13 @@ public class JsclMessageTest {
 
     @Test
     public void testTranslation() throws Exception {
-        String localizedMessage = new JsclMessage(Messages.msg_1, MessageType.error).getLocalizedMessage(Locale.ENGLISH);
-        Assert.assertTrue(localizedMessage.startsWith("Parsing error "));
+        String localizedMessage = new JsclMessage(msg_1, error).getLocalizedMessage(ENGLISH);
+        assertTrue(localizedMessage.startsWith("Parsing error "));
     }
+
+	@Test
+	public void testShouldContainPolishStrings() throws Exception {
+		String localizedMessage = new JsclMessage(msg_1, error).getLocalizedMessage(new Locale("pl", "PL"));
+		assertTrue(localizedMessage.startsWith("Wystąpił błąd "));
+	}
 }
